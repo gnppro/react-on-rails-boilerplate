2ef2576643db59ea3effae2194403072cb85622e
gnppro <gerardo.nava.pereda@gmail.com>
2021-07-28 01:03:48 -0500

    🎨 [Improve] format of the code

--- app/javascript/store/create.js
+++ app/javascript/store/create.js
@@ -9,6 +9,7 @@ const enhancers = [];
 const middleware = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line no-underscore-dangle
   const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
 
   if (typeof devToolsExtension === 'function') {
