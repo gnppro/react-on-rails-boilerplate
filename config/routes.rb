f66e6ec3039b786501127837e30a5318f9edbaf0
gnppro <gerardo.nava.pereda@gmail.com>
2021-07-28 17:19:32 -0500

    ✨ [New] devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  devise_for :users
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # resources :test, except: %i[ edit new ]
