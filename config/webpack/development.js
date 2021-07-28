6802346744c2d641e99aaef8a41dfd63efb5f2ff
gnppro <gerardo.nava.pereda@gmail.com>
2021-07-28 01:03:15 -0500

    🔧 [Config] ESLintPlugin

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,11 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const environment = require('./environment')
+const ESLintPlugin = require('eslint-webpack-plugin');
+const environment = require('./environment');
 
-module.exports = environment.toWebpackConfig()
+environment.plugins.prepend(
+  'EslintWebpack',
+  new ESLintPlugin({}),
+);
+
+module.exports = environment.toWebpackConfig();
